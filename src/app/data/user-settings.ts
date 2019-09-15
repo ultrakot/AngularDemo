df8846ee9fde06156ab3c379856bf5d21e37ff59
rozaliava <rozaliava@ROZALIA-HHS>
2019-09-15 18:34:17 +0300

    angular demo

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface UserSettings {
+    name: string,
+    emailOffers: boolean,
+    theSelect: string,
+    quibblerSubscription: string,
+    notes: string
+}
---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+
+  originalUserSettings : UserSettings = {
+    name:'milton',
+    emailOffers:true,
+    theSelect:'hush-puppy',
+    quibblerSubscription: 'Annual',
+    notes:'here we add notes...'
+  };
+
+  userSetting : UserSettings = { ...this.originalUserSettings };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
